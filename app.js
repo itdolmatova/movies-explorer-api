65cf763a38ec6e248b1a959838cf37860aa398cf
Anastasya D <itdolmatova@gmail.com>
2022-09-27 16:34:24 +0300

    <refact>: fix signup

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post('/signin', celebrate({
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
